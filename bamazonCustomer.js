da02e597e01ec3b870526dcd57ae0065a6c4fcf0
robertalanis <rxalanis@gmail.com>
2020-04-17 00:21:32 -0500

    added display products function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,7 @@ const chalk = require('chalk');
 var inquirer = require('inquirer');
 var numeral = require('numeral');
 var Table = require('cli-table3');
+var colors = require('colors');
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
@@ -18,9 +19,27 @@ var connection = mysql.createConnection({
   database: "bamazon_DB"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("success!")
+connection.connect(function(err){
+	if(err)throw err;
+	console.log("success! welome to bamazon. \nconnected as id" + connection.threadId);
 });
+
+var displayProducts = function(){
+	var query = "Select * FROM products";
+	connection.query(query, function(err, res){
+		if(err) throw err;
+		var displayTable = new Table ({
+			head: ["Item ID".green, "Product Name".green, "Catergory".green, "Price".green, "Quantity".green]
+		});
+		for(var i = 0; i < res.length; i++){
+			displayTable.push(
+				[res[i].item_id,res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+				);
+		}
+		console.log(displayTable.toString());
+	});
+}
+
+displayProducts();
 
 
