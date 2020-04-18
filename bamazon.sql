0ed961a76aa2357e9291ee38e7a1e5adb8362b0c
robertalanis <rxalanis@gmail.com>
2020-04-17 22:34:08 -0500

    welcome and exit functions

--- bamazon.sql
+++ bamazon.sql
@@ -1,21 +1,30 @@
 DROP DATABASE IF EXISTS bamazon_DB;
-
 CREATE DATABASE bamazon_DB;
 
 USE bamazon_DB;
 
-CREATE TABLE products (
+CREATE TABLE products
+(
   item_id INT NOT NULL AUTO_INCREMENT,
-  product_name VARCHAR(45) NOT NULL,
-  department_name VARCHAR(45) NOT NULL,
-  price DECIMAL(10,2) NOT NULL,
+  product_name VARCHAR (45) NOT NULL,
+  department_name VARCHAR (45) NOT NULL,
+  price DECIMAL (10,2) NOT NULL,
   stock_quantity INT NOT NULL,
   PRIMARY KEY (item_id)
 );
 
-SELECT * FROM products;
+  ALTER TABLE products AUTO_INCREMENT=1000;
 
--- INSERT INTO products (flavor, price, quantity)
--- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
+  INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+  VALUES
+    ("toilet paper", "health & personal care", 20.85, 10),
+    ("hand sanitizer", "health & personal care", 14.52, 150),
+    ("clorox bleach", "health & personal care", 11.82, 453),
+    ("face mask", "fashion", 12.56, 347),
+    ("running shoes", "fashion", 121.56, 487),
+    ("nintendo switch", "electronics", 349.99, 148),
+    ("web camera", "electronics", 49.99, 548);
 
-ALTER TABLE procuts AUTO_INCREMENT=100;
+  SELECT *
+  FROM products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,10 @@
-const chalk = require('chalk');
-var inquirer = require('inquirer');
-var numeral = require('numeral');
-var Table = require('cli-table3');
-var colors = require('colors');
+const chalk = require("chalk");
+var inquirer = require("inquirer");
+var numeral = require("numeral");
+var Table = require("cli-table3");
+var colors = require("colors");
 var mysql = require("mysql");
+var clear = require("clear");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -16,30 +17,72 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "bootcamp",
-  database: "bamazon_DB"
+  database: "bamazon_DB",
 });
 
-connection.connect(function(err){
-	if(err)throw err;
-	console.log("success! welome to bamazon. \nconnected as id" + connection.threadId);
+connection.connect(function (err) {
+  clear();
+  greetCustomer();
 });
 
-var displayProducts = function(){
-	var query = "Select * FROM products";
-	connection.query(query, function(err, res){
-		if(err) throw err;
-		var displayTable = new Table ({
-			head: ["Item ID".green, "Product Name".green, "Catergory".green, "Price".green, "Quantity".green]
-		});
-		for(var i = 0; i < res.length; i++){
-			displayTable.push(
-				[res[i].item_id,res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
-				);
-		}
-		console.log(displayTable.toString());
-	});
+function greetCustomer() {
+  inquirer
+    .prompt([
+      {
+        name: "enterStore",
+        type: "list",
+        message: "Welcome to " + "BAMAZON".rainbow +" Would you like to make a purchase?",
+        choices: ["yes", "no"],
+      },
+    ])
+    .then(function (answers) {
+      if (answers.enterStore === "yes") {
+        displayProducts()
+      }
+      else {exit()}
+    });
 }
 
-displayProducts();
+
+var displayProducts = function () {
+  var query = "Select * FROM products";
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    var displayTable = new Table({
+      head: [
+        "Item ID".green,
+        "Product Name".green,
+        "Catergory".green,
+        "Price".green,
+        "Quantity".green,
+      ],
+    });
+    for (var i = 0; i < res.length; i++) {
+      displayTable.push([
+        res[i].item_id,
+        res[i].product_name,
+        res[i].department_name,
+        res[i].price,
+        res[i].stock_quantity,
+      ]);
+    }
+    console.log(displayTable.toString());
+  });
+};
 
 
+function exit() {
+  connection.end(function(err) {
+    //Clear console
+    clear();
+    //Thank you message
+    var thankYouTable = new Table({ wordWrap: true });
+    thankYouTable.push([
+      {
+        hAlign: "center",
+        content: ("Thanks for visting!".random),
+      },
+    ]);
+    console.log(thankYouTable.toString());
+  });
+}
